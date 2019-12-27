352d9cab98ab84e5e86821542db030d0adae33db
TanyaEf <efimenkot@gmail.com>
2019-12-27 18:43:59 +0200

    Implemented client CRUD methods, added tests

---
+++ src/test/java/com/course/resttemplatedemo/client/BreweryClientTest.java
@@ -0,0 +1,45 @@
+package com.course.resttemplatedemo.client;
+
+import com.course.resttemplatedemo.web.model.BeerDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.net.URI;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class BreweryClientTest {
+
+    @Autowired
+    BreweryClient breweryClient;
+
+    @Test
+    void getBeerById() {
+        BeerDto beerDto = breweryClient.getBeerById(UUID.randomUUID());
+        assertNotNull(beerDto);
+    }
+
+    @Test
+    void saveNewBeer() {
+        BeerDto beerDto = BeerDto.builder().build();
+        URI uri = breweryClient.saveNewBeer(beerDto);
+        assertNotNull(beerDto);
+        System.out.println(uri.toString());
+    }
+
+    @Test
+    void updateBeer() {
+        BeerDto beerDto = BeerDto.builder().beerName("new name").build();
+        breweryClient.updateBeer(UUID.randomUUID(), beerDto);
+    }
+
+    @Test
+    void deleteBeer() {
+        breweryClient.deleteBeer(UUID.randomUUID());
+    }
+
+
+}
